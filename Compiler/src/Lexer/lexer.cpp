0cdf6dfd98e13a3cfba777a042e8c28c73abe5bc
assembly <morten.h.f.k@gmail.com>
2023-08-13 14:34:22 +0200

    Initial Commit with a required workflow for Pull requests that must pass before a pull request can be merged.

---
+++ Compiler/src/Lexer/lexer.cpp
@@ -0,0 +1,13 @@
+#include "Lexer/lexer.hpp"
+
+#include <string>
+
+Lexer::Lexer() {
+}
+
+Lexer::~Lexer() {
+}
+
+std::string Lexer::lex(std::string input) {
+    return input;
+}
---
+++ Compiler/test/Lexer/lexerTest.cpp
@@ -0,0 +1,5 @@
+#include <catch2/catch.hpp>
+
+TEST_CASE("Lexer Test 2", "[lexer]") {
+    REQUIRE(1 == 1);
+}

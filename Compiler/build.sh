98ff1c0c4bad8b8883e3f342deefab01e781f818
assembly <morten.h.f.k@gmail.com>
2023-08-28 09:26:49 +0200

    added code coverage, formatter - which both fail the build on a fail.

--- Compiler/build.sh
+++ Compiler/build.sh
@@ -1,32 +1,95 @@
 #!/bin/bash
+execute() {
+  local cmd_to_run="$1"
+  local errmsg="$2"
+  eval "$cmd_to_run" || { echo "Error: ${errmsg:-Execution failed}"; exit 1; }
+}
 
 setup_dependencies() {
   echo "Setting up dependencies..."
-  sudo apt-get update
-  sudo apt-get install -y cmake ninja-build catch2
+  execute "sudo apt-get update" "Failed to update apt-get"
+  execute "sudo apt-get install -y cmake ninja-build catch2 lcov gcc g++ bc" "Failed to install dependencies"
+  echo "Dependencies set up"
 }
 
-build() {
-  echo "Building compiler..."
-  [ -d build ] || mkdir build
+build_project() {
+  echo "Building project..."
+  local build_type="${1:-Debug}"
+  [ -d build ] || execute "mkdir -p build" "Failed to create build directory"
+  
+  echo "Generating compile_commands.json..."
+  echo "Generating ninja build files..."
+  execute "cmake -DCMAKE_BUILD_TYPE=${build_type} -GNinja -S . -B build" "Failed to configure project"
+  
+  execute "pushd build > /dev/null" ""
+  echo "Compiling project with ninja..."
+  execute "ninja" "Failed to build project"
+  execute "popd > /dev/null" ""
 
-  cmake -GNinja -S . -B build || { echo "CMake failed"; exit 1;}
 
-  pushd build > /dev/null
-  ninja || { echo "Ninja build failed"; exit 1;}
-  popd > /dev/null
+  echo "Project built completed"
 }
 
-test_compiler() {
-  echo "Testing compiler..."
-  pushd build > /dev/null
-  ./tests || { echo "Tests failed"; exit 1;}
-  popd > /dev/null
+test_project() {
+  echo "Testing project..."
+  execute "./build/tests" "Failed to run tests"
+  echo "Project tested"
 }
 
-clean() {
-  echo "Cleaning build..."
-  rm -rf build
+test_project_with_coverage() {
+  echo "Testing project with coverage..."
+
+  test_project
+
+  echo "Generating coverage report..."
+  execute "lcov --capture --directory . --output-file coverage.info" "Failed to generate coverage report"
+  execute "lcov --remove coverage.info '*/usr/*' '*/tests/*' --output-file coverage.filtered.info" "Failed to generate coverage report"
+  echo "Coverage report generated"
+
+  echo "Evaluate coverage report..."
+  local line_coverage=$(lcov --summary coverage.filtered.info | grep lines | awk '{print $2}' | sed 's/%//')
+  local is_coverage_good=$(echo "${line_coverage} < 100" | bc)
+  if [ "$is_coverage_good" -eq 0 ]; then
+    echo "Coverage is ${line_coverage}%"
+  else
+    echo "Coverage is ${line_coverage}%, which is less than 100%"
+    exit 1
+  fi
+  echo "Coverage report evaluated"
+
+  execute "genhtml coverage.filtered.info --output-directory coverage" "Failed to generate coverage report"
+  echo "Project test completed"
+}
+
+check_format() {
+  echo "Checking format..."
+  for file in $(find . \( -iname '*.cpp' -o -iname '*.hpp' \) -not -path './build/*'); do
+    fileDiff=$(clang-format -style=llvm $file | diff -u $file -)
+
+    if [ ! -z "$fileDiff" ]; then
+      echo "File ${file} is not formatted correctly at:"
+      echo "$fileDiff"
+      exit 1
+    fi
+  done
+  echo "Format checked"
+}
+
+clean_project() {
+  echo "Cleaning project..."
+  execute "rm -rf build" "Failed to remove build directory"
+  execute "rm -rf compile_commands.json" "Failed to remove compile_commands.json"
+  echo "Project cleaned"
+}
+
+print_help() {
+  echo "Usage: ./build.sh [command]"
+  echo "Commands:"
+  echo "  setup             - Install dependencies."
+  echo "  test              - Build and run tests."
+  echo "  test_with_coverage - Run tests and generate coverage."
+  echo "  clean             - Clean the project."
+  echo "  help              - Print this help message."
 }
 
 case "$1" in 
@@ -34,13 +97,23 @@ case "$1" in
     setup_dependencies
     ;;
   test)
-    build
-    test_compiler
+    build_project
+    test_project
+    ;;
+  test_with_coverage)
+    build_project "Coverage"
+    test_project_with_coverage
+    ;;
+  check_format)
+    check_format
     ;;
   clean)
-    clean
+    clean_project
+    ;;
+  help)
+    print_help
     ;;
   *)
-    build
+    build_project
     ;;
 esac
--- Compiler/test/Lexer/lexerTest.cpp
+++ Compiler/test/Lexer/lexerTest.cpp
@@ -1,5 +1,9 @@
+#include "Lexer/lexer.hpp"
 #include <catch2/catch.hpp>
+#include <string>
 
 TEST_CASE("Lexer Test 2", "[lexer]") {
-    REQUIRE(1 == 1);
+  Lexer lexer;
+  std::string input = "int main() { return 0; }";
+  REQUIRE(lexer.lex(input) == "int main() { return 0; }");
 }

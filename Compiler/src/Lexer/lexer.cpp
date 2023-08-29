e97a2c1fda326c6e6e72c969c321e7b070b999a0
assembly <morten.h.f.k@gmail.com>
2023-08-30 00:59:24 +0200

    Added a lexer with plus and integer tokenization, also fixed some things with build and added a format function that can auto format all source files

--- Compiler/src/Lexer/lexer.cpp
+++ Compiler/src/Lexer/lexer.cpp
@@ -1,9 +1,38 @@
 #include "Lexer/lexer.hpp"
+#include "Token/token.hpp"
 
+#include <regex>
 #include <string>
+#include <vector>
 
-Lexer::Lexer() {}
+void Lexer::advance(int amount) {
+  position += amount;
+  column += amount;
+}
 
-Lexer::~Lexer() {}
+Token Lexer::getNextToken() {
+  std::string value = "";
+  while (position <= source.length() && isspace(source[position])) {
+    position++;
+    column++;
+  }
 
-std::string Lexer::lex(std::string input) { return input; }
+  if (position <= source.length()) {
+    std::vector<std::pair<std::regex, TokenType>> tokenPatterns = {
+        {std::regex("\\b0\\b|^([1-9][0-9]*)"), TokenType::INTEGER},
+        {std::regex("^\\+"), TokenType::PLUS},
+    };
+
+    std::smatch match;
+    for (const auto &tokenPattern : tokenPatterns) {
+      if (std::regex_search(source.begin() + position, source.end(), match,
+                            tokenPattern.first)) {
+        value = match.str();
+        advance(value.length());
+        return Token(tokenPattern.second, value, line, column);
+      }
+    }
+  }
+
+  return Token(TokenType::UNKNOWN, "", line, column);
+}

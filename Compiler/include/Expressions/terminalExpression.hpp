fe89075c06dcdeae6f0ce23d14e501ef0e6ae907
SDyrbye <sunedyrbye@yahoo.dk>
2023-10-04 12:21:16 +0200

    add types to terminal expression

--- Compiler/include/Expressions/terminalExpression.hpp
+++ Compiler/include/Expressions/terminalExpression.hpp
@@ -5,6 +5,8 @@
 enum TerminalType {
   INT, 
   BOOLEAN, 
+  FLOAT,
+  CHAR,
 };
 
 class TerminalExpression : public BaseExpression {
@@ -12,12 +14,18 @@ private:
   TerminalType type;
   int intValue;
   bool boolValue;
+  float floatValue;
+  char charValue;
 
 public:
   TerminalExpression(int value) : type(INT), intValue(value) {}
   TerminalExpression(bool value) : type(BOOLEAN), boolValue(value) {}
+  TerminalExpression(float value) : type(FLOAT), floatValue(value) {}
+  TerminalExpression(char value) : type(CHAR), charValue(value) {}
   int getIntValue() { return intValue; }
   bool getBoolValue() { return boolValue; }
+  float getFloatValue() { return floatValue; }
+  char getCharValue() { return charValue; }
   TerminalType getType() { return type; }
   void accept(LLVM_Visitor *visitor) override;
 };
